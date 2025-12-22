84be36aa36f225dd5dafdd3a9f71a118cbc2520d
SRD7-Bl <2281013390@qq.com>
2025-12-22 08:52:53 +0800

    Initial Commit

---
+++ Personal project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Personal project
+//
+//  Created by Leo Yang on 2025-12-22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
